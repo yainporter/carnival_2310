e3dc30b6d39dc46dced310a9f5dddf63ae8c91f0
Yain Porter <yainporter@gmail.com>
2023-11-14 10:21:13 -0700

    test: Create the test for #summary

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -70,4 +70,50 @@ RSpec.describe Carnival do
     expect(carnival.total_revenue).to eq 5
   end
 
+  it 'can provide a summary' do
+    carnival = Carnival.new(3)
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 24, admission_fee: 2, excitement: :gentle })
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor1.add_preference(:gentle)
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor2.add_preference(:gentle)
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+    ride2.board_rider(visitor1)
+
+    expected = {
+      visitor_count: 2,
+      revenue_earned: 5,
+      visitors: [
+      {
+        visitor: visitor1,
+        favorite_ride: Ride1,
+        total_money_spent: 3
+      },
+      {
+        visitor: visitor2,
+        favorite_ride: Ride1,
+        total_money_spent: 2
+      }],
+      rides: [
+        {
+          ride: ride1,
+          riders: [visitor1, visitor2],
+          total_revenue: 3
+        },
+        {
+          ride: ride2,
+          riders: [visitor1],
+          total_revenue: 2
+        }]
+    }
+
+    expect(carnival.summary).to be_a Hash
+    expect(carnival.summary).to eq (expected)
+  end
+
 end
